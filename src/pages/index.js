11a4522ef3428073242ca6806c94e96a6df4d966
Claudio Iury <iurylemos4@gmail.com>
2021-06-04 18:20:32 -0300

    applying and settings material-ui with next

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 export default function Home() {
-  return <div>Clone Youtube usando NextJS</div>;
+  return (
+    <div>
+      Clone Youtube usando NextJS
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </div>
+  );
 }
