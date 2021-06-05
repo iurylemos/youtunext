6fb0a5c1e95a8f506679e8710a970b31d545eb29
Claudio Iury <iurylemos4@gmail.com>
2021-06-05 07:45:16 -0300

    created connection with database mongodb

---
+++ src/utils/mongodb.js
@@ -0,0 +1,37 @@
+import { MongoClient } from 'mongodb';
+
+const URI = process.env.MONGODB_URI;
+const dbName = process.env.MONDGO_DB;
+
+let cacheDb;
+let cacheClient;
+
+if (!URI) {
+  throw new Error(
+    'Please define the MONGODB_UI enviroment variable inside .env.local',
+  );
+}
+
+if (!dbName) {
+  throw new Error(
+    'Please define the MONGO_DB enviroment variable inside .env.local',
+  );
+}
+
+export default async function connectDatabase() {
+  if (cacheDb && cacheClient) {
+    return { cleint: cacheClient, db: cacheDb };
+  }
+
+  const client = await MongoClient.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  const db = await client.db(dbName);
+
+  cacheClient = client;
+  cacheDb = db;
+
+  return { client, db };
+}
